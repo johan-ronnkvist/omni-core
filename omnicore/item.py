4f45c43feefb3f3c4ff8e0bceae97face3a9802c
Johan Rönnkvist <johan.ronnkvist@gmail.ccom>
2023-03-02 14:55:48 +0100

    Some changes

--- omnicore/item.py
+++ omnicore/item.py
@@ -1,13 +1,16 @@
 import time
+import datetime
 
-
-class Item():
+class Item:
     def __init__(self, number: int):
         self._number = number
 
     @property
     def num(self):
         return self._number
+
+    def time(self):
+        return datetime.time.second - self.num
 
 
 print("test")
